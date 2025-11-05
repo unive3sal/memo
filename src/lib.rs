67478580ce9cd9fdc732e381e9e6c1e4723f249c
metaCoder-00 <dengyuhao.233@outlook.com>
2025-11-05 19:29:58 +0800

    add entrypoint

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
 use solana_program::entrypoint;
 
+use self::processor::process_instruction;
+
 pub mod error;
 pub mod instruction;
 pub mod processor;
 pub mod state;
+
+entrypoint!(process_instruction);
